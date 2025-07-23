aae80f891624b4e0dcf7d14051cf9b27365dd72c
Throin <xthroin@gmail.com>
2025-07-23 04:41:58 +0200

    Add AuthenticationService with OAuth2 support
    Introduces AuthenticationService to handle OAuth2 authentication using angular-oauth2-oidc. Includes login, logout, token validation, and username retrieval methods, along with a basic unit test for service creation.

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { authCodeFlowConfig } from '../app.config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor(private oAuthService: OAuthService,
+    private readonly router: Router) {
+    this.oAuthService.configure(authCodeFlowConfig);
+
+    this.initializeAuthentication();
+
+    this.oAuthService.setupAutomaticSilentRefresh();
+  }
+
+  /**
+   * Initializes the authentication process.
+   * This method loads the discovery document and attempts to log in the user.
+   * If the user is redirected back to the application after login, it navigates to the original URL. 
+   */
+  private initializeAuthentication(): void {
+    this.oAuthService.loadDiscoveryDocumentAndTryLogin().then(() => {
+      const url = this.oAuthService.state;
+
+      if (url) {
+        this.router.navigateByUrl(decodeURIComponent(url));
+      }
+    });
+  }
+
+  /**
+   * Initiates the login process by redirecting the user to the OAuth server.
+   * The user will be redirected back to the application after successful login.
+   */
+  login(): void {
+    this.oAuthService.initCodeFlow(window.location.pathname);
+  }
+
+  /**
+   * Logs out the user and redirects them to the home page.
+   */
+  logout(): void {
+    this.oAuthService.logOut();
+  }
+
+  /**
+   * Checks if the user has a valid token.
+   * @returns {boolean} True if the user has a valid ID token and access token, false otherwise.
+   */
+  hasValidToken(): boolean {
+    return this.oAuthService.hasValidIdToken() && this.oAuthService.hasValidAccessToken();
+  }
+
+  /**
+   * Retrieves the username of the authenticated user.
+   * @returns {string | undefined} The username if available, otherwise undefined.
+   */
+  getUsername(): string | undefined {
+    return this.oAuthService.getIdentityClaims()?.['name'];
+  }
+}
