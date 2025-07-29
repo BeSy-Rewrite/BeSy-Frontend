ef5ac0c8d332c172e8074e5a2fba80e00b5558e8
Throin <xthroin@gmail.com>
2025-07-30 01:58:30 +0200

    Add generic table model interfaces
    Introduces TableColumn and TableActionButton interfaces, along with the ButtonColor enum, to support generic table structures and actions in the application.

---
+++ src/app/models/generic-table.ts
@@ -0,0 +1,23 @@
+import { MatButtonAppearance } from "@angular/material/button";
+
+
+export interface TableColumn {
+    id: string;
+    label: string;
+    isUnsortable?: boolean;
+}
+
+export interface TableActionButton {
+    id: string;
+    label: string;
+    buttonType?: MatButtonAppearance;
+    color?: ButtonColor;
+    action?: (row: any) => void;
+}
+
+export enum ButtonColor {
+    PRIMARY = 'primary',
+    ACCENT = 'accent',
+    WARN = 'warn',
+    DEFAULT = 'default'
+}
