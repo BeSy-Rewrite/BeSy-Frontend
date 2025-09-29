a77dae7f36fca7f3dae106736dc461f71e161f94
Thr0in <xthroin@gmail.com>
2025-09-30 01:25:57 +0200

    Refactor orders table columns to use config
    Replaces hardcoded ordersTableColumns with imported ordersTableConfig for improved maintainability. Sets view action for specific columns and removes edit/delete actions and related methods from the component.

--- src/app/pages/order/orders-page/orders-page.component.ts
+++ src/app/pages/order/orders-page/orders-page.component.ts
@@ -7,6 +7,7 @@ import { MatTab, MatTabGroup } from '@angular/material/tabs';
 import { Router } from '@angular/router';
 import { FilterMenuComponent } from '../../../components/filter-menu/filter-menu.component';
 import { GenericTableComponent } from '../../../components/generic-table/generic-table.component';
+import { ordersTableConfig } from '../../../configs/orders-table-config';
 import { ActiveFilters } from '../../../models/filter-menu-types';
 import { ButtonColor, TableActionButton } from '../../../models/generic-table';
 import { OrdersDataSourceService } from '../../../services/orders-data-source.service';
@@ -27,26 +28,19 @@ import { OrdersDataSourceService } from '../../../services/orders-data-source.se
   styleUrl: './orders-page.component.css'
 })
 export class OrdersPageComponent {
-  ordersTableColumns = [
-    { id: 'id', label: 'Bestellnummer', action: (row: any) => this.onViewOrder(row) },
-    { id: 'status', label: 'Status' },
-    { id: 'content_description', label: 'Beschreibung' },
-    { id: 'primary_cost_center_id', label: 'Kostenstelle' },
-    { id: 'supplier_id', label: 'Lieferant' },
-    { id: 'last_updated_time', label: 'Änderungsdatum' }
+  ordersTableColumns = [...ordersTableConfig];
 
-  ];
   ordersDataSource = inject(OrdersDataSourceService);
   actions: TableActionButton[] = [
-    { id: 'delete', label: 'Löschen', buttonType: 'outlined', color: ButtonColor.WARN, action: (row) => this.onDeleteOrder(row) },
-    { id: 'edit', label: 'Bearbeiten', buttonType: 'outlined', color: ButtonColor.PRIMARY, action: (row) => this.onEditOrder(row) },
     { id: 'view', label: 'Ansehen', buttonType: 'filled', color: ButtonColor.PRIMARY, action: (row) => this.onViewOrder(row) }
-
   ];
 
   showFilters = false;
 
-  constructor(private readonly router: Router) { }
+  constructor(private readonly router: Router) {
+    ordersTableConfig.filter(col => ['id', 'besy_number'].includes(col.id))
+      .forEach(col => col.action = (row) => this.onViewOrder(row));
+  }
 
   toggleShowFilters() {
     this.showFilters = !this.showFilters;
@@ -59,14 +53,5 @@ export class OrdersPageComponent {
   onViewOrder(order: any) {
     console.log('Viewing order:', order);
     this.router.navigate(['/orders', order.id]);
-  }
-
-  onEditOrder(order: any) {
-    this.router.navigate(['/orders', order.id, 'edit']);
-  }
-
-  onDeleteOrder(order: any) {
-    // ToDo: Implement order deletion logic here
-    console.log('Deleting order:', order);
   }
 }
