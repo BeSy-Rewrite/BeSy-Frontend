85e7ee83e3aae82a007443bb26f893cbd6b02805
Thr0in <xthroin@gmail.com>
2025-09-30 21:21:05 +0200

    Add OrderDetailsComponent for order display
    Introduces the OrderDetailsComponent with template, styles, and unit test. The component displays key order fields using provided labels and order data, supporting improved order detail visualization.

---
+++ src/app/components/order-details/order-details.component.ts
@@ -0,0 +1,28 @@
+import { Component, input } from '@angular/core';
+import { ORDER_FIELD_LABELS } from '../../display-name-mappings/order-names';
+import { DisplayableOrder } from '../../models/displayable-order';
+import { OrderDisplayData } from '../../models/order-display-data';
+
+@Component({
+  selector: 'app-order-details',
+  imports: [],
+  templateUrl: './order-details.component.html',
+  styleUrl: './order-details.component.scss'
+})
+export class OrderDetailsComponent {
+
+  orderData = input.required<DisplayableOrder>();
+  fieldLabels = ORDER_FIELD_LABELS;
+
+  visibleFields: (keyof OrderDisplayData)[] = [
+    'primary_cost_center_id',
+    'secondary_cost_center_id',
+    'booking_year',
+    'auto_index',
+    'legacy_alias',
+    'dfg_key',
+    'created_date',
+    'last_updated_time',
+  ];
+
+}
