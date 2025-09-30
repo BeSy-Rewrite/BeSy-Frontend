5f87471154efaeff5e3ec419c32b2d109c06ad5d
Thr0in <xthroin@gmail.com>
2025-09-30 21:22:03 +0200

    Redesign order view page with tabs and details
    Replaces placeholder content with a structured layout including tabs for order details and associated persons. Integrates OrderDetails and PersonDetails components, adds routing for editing, and maps person roles to display names for improved clarity.

--- src/app/pages/order/view-order-page/view-order-page.component.ts
+++ src/app/pages/order/view-order-page/view-order-page.component.ts
@@ -1,15 +1,44 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from "@angular/material/divider";
+import { MatTabsModule } from "@angular/material/tabs";
+import { RouterModule } from '@angular/router';
 import { OrderResponseDTO } from '../../../api';
-import { JsonPipe } from '@angular/common';
+import { OrderDetailsComponent } from '../../../components/order-details/order-details.component';
+import { PersonDetailsComponent } from "../../../components/person-details/person-details.component";
+import { ORDER_FIELD_LABELS } from '../../../display-name-mappings/order-names';
+import { DisplayableOrder } from '../../../models/displayable-order';
 
 @Component({
   selector: 'app-view-order-page',
   imports: [
-    JsonPipe
+    RouterModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatTabsModule,
+    OrderDetailsComponent,
+    PersonDetailsComponent
   ],
   templateUrl: './view-order-page.component.html',
   styleUrl: './view-order-page.component.scss'
 })
-export class ViewOrderPageComponent {
-  order = input.required<OrderResponseDTO>();
+export class ViewOrderPageComponent implements OnInit {
+  order = input.required<DisplayableOrder>();
+  orderFieldNames = ORDER_FIELD_LABELS;
+
+  personKeys = ['owner_id', 'delivery_person_id', 'invoice_person_id', 'queries_person_id'];
+  personMappings = new Map<number, string[]>();
+
+  ngOnInit(): void {
+    for (const key of this.personKeys) {
+      const personId = this.order().order[key as keyof OrderResponseDTO] as number | undefined;
+      if (personId == undefined) continue;
+
+      if (!this.personMappings.has(personId)) {
+        this.personMappings.set(personId, [key]);
+      } else {
+        this.personMappings.get(personId)?.push(key);
+      }
+    }
+  }
 }
