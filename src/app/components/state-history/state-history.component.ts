59ea52884ae57b8eb4989595c3a47a6496d66304
Throin <xthroin@gmail.com>
2025-10-02 11:21:49 +0200

    Add StateHistoryComponent for order status history
    Introduces StateHistoryComponent to display the current order status and its history in a table. Includes HTML template, SCSS file, TypeScript logic for fetching and formatting status history, and a basic test to verify component creation.

---
+++ src/app/components/state-history/state-history.component.ts
@@ -0,0 +1,44 @@
+import { Component, input } from '@angular/core';
+import { OrderResponseDTO, OrdersService, OrderStatusHistoryResponseDTO } from '../../api';
+import { OrdersWrapperService } from '../../services/wrapper-services/orders-wrapper.service';
+import { STATE_HISTORY_FIELD_NAMES } from '../../display-name-mappings/state-history-names';
+import { statusDisplayNames, statusIcons } from '../../display-name-mappings/status-names';
+import { GenericTableComponent } from "../generic-table/generic-table.component";
+import { DataSource } from '@angular/cdk/table';
+import { MatTableDataSource } from '@angular/material/table';
+import { TableColumn } from '../../models/generic-table';
+import { timestamp } from 'rxjs';
+
+@Component({
+  selector: 'app-state-history',
+  imports: [GenericTableComponent],
+  templateUrl: './state-history.component.html',
+  styleUrl: './state-history.component.scss'
+})
+export class StateHistoryComponent {
+  order = input.required<OrderResponseDTO>();
+  stateHistory: OrderStatusHistoryResponseDTO[] = [];
+  
+  stateHistoryFieldNames = STATE_HISTORY_FIELD_NAMES;
+  stateNames = statusDisplayNames;
+  stateIcons = statusIcons;
+
+  dataSource: DataSource<{ status: string; timestamp: string }> = new MatTableDataSource();
+  columns: TableColumn[] = [
+    { id: 'timestamp', label: this.stateHistoryFieldNames['timestamp'] },
+    { id: 'status', label: this.stateHistoryFieldNames['status'] }
+  ];
+
+  constructor(private readonly ordersService: OrdersWrapperService) {}
+
+  fetchStateHistory(): void {
+    this.ordersService.getOrderStatusHistory(this.order().id ?? 0).then(history => {
+      this.stateHistory = history;
+      const formattedHistory = this.stateHistory.map(entry => ({
+        status: `${this.stateIcons.get(entry.status.toString() ?? '')} ${this.stateNames.get(entry.status.toString() ?? '')}`,
+        timestamp: new Date(entry.timestamp).toLocaleString()
+      }));
+      this.dataSource = new MatTableDataSource(formattedHistory);
+    });
+  }
+}
