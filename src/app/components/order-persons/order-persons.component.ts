5b04c73aac0b1683cfa362883f006ec741a17188
Thr0in <luweit01@hs-esslingen.de>
2025-10-09 19:21:25 +0200

    Refactor order persons display into new component
    Moved logic and UI for displaying order-related persons from ViewOrderPageComponent into a new OrderPersonsComponent. Updated view-order-page to use the new component, improving modularity and maintainability. Added tests and styles for OrderPersonsComponent.

---
+++ src/app/components/order-persons/order-persons.component.ts
@@ -0,0 +1,58 @@
+import { Component, input, OnInit } from '@angular/core';
+import { OrderResponseDTO } from '../../api';
+import { ORDER_FIELD_LABELS } from '../../display-name-mappings/order-names';
+import { PersonDetailsComponent } from '../person-details/person-details.component';
+
+type Gender = 'm' | 'f' | 'd';
+
+@Component({
+  selector: 'app-order-persons',
+  imports: [
+    PersonDetailsComponent
+  ],
+  templateUrl: './order-persons.component.html',
+  styleUrl: './order-persons.component.scss'
+})
+export class OrderPersonsComponent implements OnInit {
+  /** The order whose persons should be displayed */
+  order = input.required<OrderResponseDTO>();
+
+  orderFieldNames = ORDER_FIELD_LABELS;
+
+  personKeys = ['delivery_person_id', 'invoice_person_id', 'queries_person_id'];
+  personMappings = new Map<number, string[]>();
+
+  personGenderMap = new Map<number, Gender>();
+
+  ngOnInit(): void {
+    for (const key of this.personKeys) {
+      const personId = this.order()[key as keyof OrderResponseDTO] as number | undefined;
+      if (personId == undefined) continue;
+
+      this.personGenderMap.set(personId ?? -1, 'd');
+
+      if (this.personMappings.has(personId)) {
+        this.personMappings.get(personId)?.push(key);
+      } else {
+        this.personMappings.set(personId, [key]);
+      }
+    }
+  }
+
+  /**
+   * Get a gendered word based on the provided gender.
+   * @param word The word to be gendered. Must contain ':in' to indicate where
+   * @param gender The gender to use for gendering the word.
+   * @returns The gendered word.
+   */
+  getGenderedWord(word: string, gender: Gender): string {
+    switch (gender) {
+      case 'f':
+        return word.replace(':in', 'in');
+      case 'm':
+        return word.replace(':in', '');
+      case 'd':
+        return word;
+    }
+  }
+}
--- src/app/pages/order/view-order-page/view-order-page.component.ts
+++ src/app/pages/order/view-order-page/view-order-page.component.ts
@@ -1,21 +1,21 @@
 import { ClipboardModule } from "@angular/cdk/clipboard";
-import { Component, computed, input, OnInit, signal } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from "@angular/material/divider";
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from "@angular/material/tabs";
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
-import { OrderResponseDTO, OrderStatus } from '../../../api';
+import { OrderStatus } from '../../../api';
 import { ApprovalsComponent } from '../../../components/approvals/approvals.component';
 import { OrderMainInformationComponent } from '../../../components/order-main-information/order-main-information.component';
+import { OrderPersonsComponent } from "../../../components/order-persons/order-persons.component";
 import { PersonDetailsComponent } from "../../../components/person-details/person-details.component";
 import { QuotationsListComponent } from '../../../components/quotations-list/quotations-list.component';
 import { StateHistoryComponent } from "../../../components/state-history/state-history.component";
 import { ORDER_FIELD_LABELS } from '../../../display-name-mappings/order-names';
 import { DisplayableOrder } from '../../../models/displayable-order';
 
-type Gender = 'm' | 'f' | 'd';
 
 @Component({
   selector: 'app-view-order-page',
@@ -29,6 +29,7 @@ type Gender = 'm' | 'f' | 'd';
     ClipboardModule,
     OrderMainInformationComponent,
     PersonDetailsComponent,
+    OrderPersonsComponent,
     QuotationsListComponent,
     ApprovalsComponent,
     StateHistoryComponent,
@@ -36,42 +37,12 @@ type Gender = 'm' | 'f' | 'd';
   templateUrl: './view-order-page.component.html',
   styleUrl: './view-order-page.component.scss'
 })
-export class ViewOrderPageComponent implements OnInit {
+export class ViewOrderPageComponent {
   order = input.required<DisplayableOrder>();
   orderFieldNames = ORDER_FIELD_LABELS;
   orderStates = OrderStatus;
 
-  personKeys = ['delivery_person_id', 'invoice_person_id', 'queries_person_id'];
-  personMappings = new Map<number, string[]>();
-
-  ordererGender = signal<Gender>('d');
-  personGenderMap = new Map<number, Gender>();
 
   currentUrl = computed(() => window.location.href);
 
-  ngOnInit(): void {
-    for (const key of this.personKeys) {
-      const personId = this.order().order[key as keyof OrderResponseDTO] as number | undefined;
-      if (personId == undefined) continue;
-
-      this.personGenderMap.set(personId ?? -1, 'd');
-
-      if (this.personMappings.has(personId)) {
-        this.personMappings.get(personId)?.push(key);
-      } else {
-        this.personMappings.set(personId, [key]);
-      }
-    }
-  }
-
-  getGenderedWord(word: string, gender: Gender): string {
-    switch (gender) {
-      case 'f':
-        return word.replace(':in', 'in');
-      case 'm':
-        return word.replace(':in', '');
-      case 'd':
-        return word;
-    }
-  }
 }
