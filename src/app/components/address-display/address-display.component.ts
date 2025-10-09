e986524d57b93e2c4853be76285186b7f3d0ce93
Thr0in <luweit01@hs-esslingen.de>
2025-10-09 20:41:22 +0200

    Add AddressDisplayComponent for address rendering
    Introduces AddressDisplayComponent with template, styles, and tests. The component fetches and displays address details based on a provided address ID using PersonsWrapperService.

---
+++ src/app/components/address-display/address-display.component.ts
@@ -0,0 +1,34 @@
+import { Component, input, OnInit, signal } from '@angular/core';
+import { AddressResponseDTO } from '../../api';
+import { PersonsWrapperService } from '../../services/wrapper-services/persons-wrapper.service';
+
+type DisplayAddress = { [key in keyof AddressResponseDTO]: string };
+
+@Component({
+  selector: 'app-address-display',
+  imports: [],
+  templateUrl: './address-display.component.html',
+  styleUrl: './address-display.component.scss'
+})
+export class AddressDisplayComponent implements OnInit {
+  /**
+   * The ID of the address to display.
+   */
+  addressId = input.required<number>();
+
+  address = signal<DisplayAddress | undefined>(undefined);
+
+  constructor(private readonly personsService: PersonsWrapperService) { }
+
+  ngOnInit(): void {
+    this.personsService.getAddress(this.addressId()).subscribe(address => {
+      const addressDisplay: DisplayAddress = {};
+
+      Object.entries(address ?? {}).forEach(([key, value]) => {
+        addressDisplay[key as keyof DisplayAddress] = typeof value === 'string' ? value : value?.toString() ?? '';
+      });
+      this.address.set(addressDisplay);
+    });
+  }
+
+}
