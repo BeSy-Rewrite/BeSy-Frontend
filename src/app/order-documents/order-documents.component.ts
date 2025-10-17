dda097e1c7f8adafc6f1aea668336b70c645bfdc
Thr0in <luweit01@hs-esslingen.de>
2025-10-17 19:20:33 +0200

    Add generic table for order documents
    Replaces placeholder content with a generic table displaying order documents. Implements data fetching, formatting, and download functionality for invoices related to an order, and sets up table columns and actions for document management.

--- src/app/order-documents/order-documents.component.ts
+++ src/app/order-documents/order-documents.component.ts
@@ -1,11 +1,119 @@
-import { Component } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableDataSource } from '@angular/material/table';
+import { InvoiceResponseDTO, OrderResponseDTO } from '../api';
+import { GenericTableComponent } from "../components/generic-table/generic-table.component";
+import { INVOICE_FIELD_NAMES } from '../display-name-mappings/invoice-names';
+import { ButtonColor, TableActionButton, TableColumn } from '../models/generic-table';
+import { OrderSubresourceResolverService } from '../services/order-subresource-resolver.service';
+import { InvoicesWrapperServiceService } from '../services/wrapper-services/invoices-wrapper-service.service';
+
+type DisplayableInvoice = Omit<InvoiceResponseDTO, 'price'> & {
+  price?: string;
+  tooltips?: { [K in keyof Partial<Omit<DisplayableInvoice, 'tooltips'>>]: string };
+};
 
 @Component({
   selector: 'app-order-documents',
-  imports: [],
+  imports: [GenericTableComponent],
   templateUrl: './order-documents.component.html',
   styleUrl: './order-documents.component.scss'
 })
-export class OrderDocumentsComponent {
+export class OrderDocumentsComponent implements OnInit {
+  // TODO: Add preview functionality
+  // TODO: Add upload functionality
+  /**
+   * The order for which to display documents.
+   */
+  order = input.required<OrderResponseDTO>();
+
+  documents: DisplayableInvoice[] = [];
+  documentPreviews = new Map<number, string>();
+
+  dataSource = new MatTableDataSource<DisplayableInvoice>(this.documents);
+  columns: TableColumn<DisplayableInvoice>[] = [
+    { id: 'comment', label: INVOICE_FIELD_NAMES.comment },
+    { id: 'cost_center_id', label: INVOICE_FIELD_NAMES.cost_center_id },
+    { id: 'price', label: INVOICE_FIELD_NAMES.price },
+    { id: 'date', label: INVOICE_FIELD_NAMES.date },
+    { id: 'created_date', label: INVOICE_FIELD_NAMES.created_date },
+    {
+      id: 'paperless_id', label: INVOICE_FIELD_NAMES.paperless_id, action(row) {
+        if (row.paperless_id)
+          navigator.clipboard.writeText(row.paperless_id?.toString());
+      },
+    },
+    { id: 'order_id', label: INVOICE_FIELD_NAMES.order_id, isInvisible: true }
+  ];
+  actions: TableActionButton[] = [
+    {
+      id: 'download',
+      label: 'Herunterladen',
+      buttonType: 'outlined',
+      color: ButtonColor.PRIMARY,
+      action: (row: DisplayableInvoice) => {
+        if (row.id)
+          this.downloadDocument(row);
+      }
+    },
+    {
+      id: 'preview',
+      label: 'Vorschau',
+      buttonType: 'outlined',
+      color: ButtonColor.PRIMARY,
+      action: (row: DisplayableInvoice) => {
+        console.log('Preview document with ID:', row.id);
+      }
+    }
+  ];
+
+  constructor(
+    private readonly invoicesService: InvoicesWrapperServiceService,
+    private readonly resourceResolverService: OrderSubresourceResolverService,
+    private readonly _snackBar: MatSnackBar
+  ) { }
+
+  /**
+   * Initializes the component by fetching and formatting the order documents.
+   */
+  ngOnInit() {
+    if (!this.order().id) {
+      this._snackBar.open('Bestellungs-ID fehlerhaft', 'Schließen', { duration: 3000 });
+      return;
+    }
+    this.invoicesService.getDocumentsByOrderId(this.order().id!).subscribe(invoices => {
+      this.documents = invoices.map(invoice => ({
+        ...invoice,
+        date: this.resourceResolverService.formatDate(invoice.date),
+        created_date: this.resourceResolverService.formatDate(invoice.created_date),
+        price: this.resourceResolverService.formatPrice(invoice.price, 'EUR'),
+        tooltips: {
+          paperless_id: invoice.paperless_id ? 'Klicken zum Kopieren der Paperless ID' : 'Keine Paperless ID vorhanden'
+        }
+      }));
+      console.log(this.documents);
+
+      this.dataSource.data = this.documents;
+    });
+  }
+
+  /**
+   * Downloads the specified document.
+   * @param row The documents meta data.
+   */
+  downloadDocument(row: DisplayableInvoice) {
+    if (!row.id) {
+      this._snackBar.open('Dokument-ID fehlerhaft', 'Schließen', { duration: 3000 });
+      return;
+    }
+    this.invoicesService.downloadDocument(row.id).subscribe(blob => {
+      const a = document.createElement('a')
+      const objectUrl = URL.createObjectURL(blob)
+      a.href = objectUrl
+      a.download = `Dokument-${row.id}_Bestellung-${row.order_id}_${row.comment}.pdf`;
+      a.click();
+      URL.revokeObjectURL(objectUrl);
+    });
+  }
 
 }
