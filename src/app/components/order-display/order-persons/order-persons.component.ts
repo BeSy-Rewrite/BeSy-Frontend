73468f1072076d9b8b7565ee4362d669de971daf
Thr0in <luweit01@hs-esslingen.de>
2025-11-07 00:55:03 +0100

    Simplify order persons display logic
    Refactored order-persons component to only show the queries person if present, removing previous logic for mapping and displaying multiple person roles. Unused code and gendered word handling were removed for clarity and maintainability. Added delivery and invoice person IDs to main information display.

--- src/app/components/order-display/order-persons/order-persons.component.ts
+++ src/app/components/order-display/order-persons/order-persons.component.ts
@@ -1,6 +1,5 @@
-import { Component, input, OnChanges, OnInit } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { OrderResponseDTO } from '../../../api';
-import { ORDER_FIELD_NAMES } from '../../../display-name-mappings/order-names';
 import { PersonDetailsComponent } from '../../person-details/person-details.component';
 
 type Gender = 'm' | 'f' | 'd';
@@ -13,52 +12,7 @@ type Gender = 'm' | 'f' | 'd';
   templateUrl: './order-persons.component.html',
   styleUrl: './order-persons.component.scss'
 })
-export class OrderPersonsComponent implements OnInit, OnChanges {
+export class OrderPersonsComponent {
   /** The order whose persons should be displayed */
   order = input.required<OrderResponseDTO>();
-
-  orderFieldNames = ORDER_FIELD_NAMES;
-
-  personKeys = ['delivery_person_id', 'invoice_person_id', 'queries_person_id'];
-  personMappings = new Map<number, string[]>();
-
-  personGenderMap = new Map<number, Gender>();
-
-  ngOnInit(): void {
-    for (const key of this.personKeys) {
-      const personId = this.order()[key as keyof OrderResponseDTO] as number | undefined;
-      if (personId == undefined) continue;
-
-      this.personGenderMap.set(personId ?? -1, 'd');
-
-      if (this.personMappings.has(personId)) {
-        this.personMappings.get(personId)?.push(key);
-      } else {
-        this.personMappings.set(personId, [key]);
-      }
-    }
-  }
-
-  ngOnChanges(): void {
-    if (this.order()) {
-      this.ngOnInit();
-    }
-  }
-
-  /**
-   * Get a gendered word based on the provided gender.
-   * @param word The word to be gendered. Must contain ':in' to indicate where
-   * @param gender The gender to use for gendering the word.
-   * @returns The gendered word.
-   */
-  getGenderedWord(word: string, gender: Gender): string {
-    switch (gender) {
-      case 'f':
-        return word.replace(':in', 'in');
-      case 'm':
-        return word.replace(':in', '');
-      case 'd':
-        return word;
-    }
-  }
 }
