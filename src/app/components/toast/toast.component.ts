953bf13c6afe0ad4d16034bbe365d67c15ad2758
Thr0in <luweit01@hs-esslingen.de>
2025-11-23 23:50:38 +0100

    Enhance toast component with position and toggle
    Refactored the toast component to support configurable position, show/hide functionality, and improved animations. Added new SCSS styles for open/closed states and fade-in effect. The component now uses signals for state management and sets up fixed positioning based on parent element.

--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, ElementRef, input, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ToastService } from '../../services/toast.service';
@@ -15,6 +15,8 @@ export interface ToastResponse extends ToastRequest {
   cancel: () => void;
 }
 
+export type ToastPosition = 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left';
+
 
 @Component({
   selector: 'app-toast',
@@ -26,12 +28,30 @@ export interface ToastResponse extends ToastRequest {
   styleUrl: './toast.component.scss'
 })
 export class ToastComponent {
+  position = input<ToastPosition>('bottom-right');
 
   toasts: ToastResponse[] = [];
 
-  constructor(private readonly toastService: ToastService) {
+  showToasts = signal(true);
+  areToastsEmpty = signal(false);
+
+  constructor(
+    private readonly toastService: ToastService,
+    private readonly elementRef: ElementRef
+  ) {
     this.toastService.toasts.subscribe(toasts => {
       this.toasts = toasts;
+      this.areToastsEmpty.set(toasts.length === 0);
+      this.showToasts.set(toasts.length > 0);
+    });
+    let position: DOMRect;
+    afterNextRender({
+      earlyRead: () => {
+        position = this.elementRef.nativeElement.parentElement.getBoundingClientRect();
+      },
+      write: () => {
+        this.setupFixedPosition(position);
+      }
     });
   }
 
@@ -50,5 +70,20 @@ export class ToastComponent {
 
   removeToast(id: number): void {
     this.toastService.removeToast(id);
+  }
+
+  setupFixedPosition(position: DOMRect): void {
+    const element = this.elementRef.nativeElement;
+
+    const horizontalPositionStyle = this.position().includes('left') ? `left: ${position.left}px;` : `right: ${window.innerWidth - position.right}px;`;
+    const verticalPositionStyle = this.position().includes('top') ? `top: ${position.top}px;` : `bottom: ${window.innerHeight - position.bottom}px;`;
+
+    element.setAttribute('style', `width: fit-content; max-height: 50vh; \
+      position: fixed; ${verticalPositionStyle} ${horizontalPositionStyle}`);
+  }
+
+  toggleToasts(): void {
+    this.showToasts.set(!this.showToasts());
+    console.log(this.showToasts());
   }
 }
