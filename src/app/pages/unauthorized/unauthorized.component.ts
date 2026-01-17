c4c244d007a053dec3b4659bff8350f792cc396d
Throin <luweit01@hs-esslingen.de>
2026-01-17 23:21:56 +0100

    Enhance unauthorized page with UI and login action
    Updated the unauthorized component to display a styled 401 error message and provide a login button. Injected AuthenticationService to handle login action from the template.

--- src/app/pages/unauthorized/unauthorized.component.ts
+++ src/app/pages/unauthorized/unauthorized.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-unauthorized',
   imports: [],
   templateUrl: './unauthorized.component.html',
-  styleUrl: './unauthorized.component.scss'
+  styleUrl: './unauthorized.component.scss',
 })
 export class UnauthorizedComponent {
-
+  constructor(protected authService: AuthenticationService) {}
 }
