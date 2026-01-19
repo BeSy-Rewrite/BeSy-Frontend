406be84558a046a7cb16fa56e3a8ff77d078ef46
Throin <luweit01@hs-esslingen.de>
2026-01-19 15:48:03 +0100

    Remove unused API version logic from UtilsService
    Eliminated the getApiVersion method and related imports from UtilsService, as they are no longer needed. This simplifies the service and removes unnecessary dependencies.

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,35 +1,15 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import JSConfetti from 'js-confetti';
-import { catchError, map, of } from 'rxjs';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UtilsService {
   private readonly jsConfetti = new JSConfetti();
-  private apiVersion: string | undefined = undefined;
 
-  constructor(private readonly http: HttpClient) { }
+  constructor() {}
 
   getConfettiInstance(): JSConfetti {
     return this.jsConfetti;
-  }
-
-  getApiVersion() {
-    if (this.apiVersion) {
-      return of(this.apiVersion);
-    }
-    return this.http.get(environment.apiVersionEndpoint).pipe(
-      map(version => {
-        this.apiVersion = (version as any)?.info?.version as string;
-        return this.apiVersion;
-      }),
-      catchError(() => {
-        this.apiVersion = undefined;
-        return of(undefined);
-      })
-    );
   }
 }
