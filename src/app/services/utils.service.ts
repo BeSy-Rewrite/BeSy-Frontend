9514bc3b7fffbd442bf5759fab27054a23ac9b0f
Dominik Ehmann <doehit00@hs-esslingen.de>
2026-02-23 01:49:13 +0100

    remove unused apiVersion property and method from UtilsService

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,21 +1,15 @@
 import { Injectable } from '@angular/core';
 import JSConfetti from 'js-confetti';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UtilsService {
   private readonly jsConfetti = new JSConfetti();
-  apiVersion = environment.apiVersion;
 
   constructor() {}
 
   getConfettiInstance(): JSConfetti {
     return this.jsConfetti;
-  }
-
-  getApiVersion(): string {
-    return this.apiVersion;
   }
 }
