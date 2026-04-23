d791ee34002e0c71f2fb59c81804da22113b7cf0
Luka Weipert <luka.weipert@iao.fraunhofer.de>
2026-04-23 15:49:40 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,14 @@ import { OrderStatus } from '../app/api-services-v2';
 export const environment = {
   production: true, // Disable authentication in non-production environments
   apiUrl: 'https://besy.hs-esslingen.com/api/v1',
-  paperlessUrl: 'https://paperless.besy.hs-esslingen.com',
+  paperlessUrl: 'https://paperless.hs-esslingen.com',
 
   footerLinks: [{ name: 'InSy', link: 'https://insy.hs-esslingen.com' }],
 
   bugReportUrl: 'https://github.com/BeSy-Rewrite/BeSy-Frontend/issues/new/choose',
 
   // Keycloak configuration
-  identityProviderUrl: 'https://auth.insy.hs-esslingen.com/realms/insy',
+  identityProviderUrl: 'https://auth.dev.hs-esslingen.com/realms/besy',
   clientId: 'besy',
   requiredRole: 'orderer',
   approveOrdersRole: 'approver',
@@ -32,10 +32,10 @@ export const environment = {
   wrappedBannerEnabled: true,
   // 0-indexed months: 11 = December, 0 = January (half-year wrap)
   // Length must be 2, first is half-year wrapped month second is full-year wrapped month
-  wrappedBannerMonths: [5, 11] as number[],
+  wrappedBannerMonths: [3, 11] as number[],
   wrappedUrl: '/wrap',
 
-  showFooter: true,
+  showFooter: false,
 
   invoicesLegacyCutoffDate: new Date('2026-01-01T00:00:00Z'),
 
